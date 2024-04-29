3ea56054c6311cc998a36ab6327803279082dbad
mdjukan <mdjukan@protonmail.com>
2024-04-29 23:13:10 +0200

    .

--- zk2019/Tabla.java
+++ zk2019/Tabla.java
@@ -1,64 +1,32 @@
 import java.awt.*;
 import java.util.ArrayList;
 
-class Tabla extends Canvas {
+class Tabla extends Canvas implements Runnable {
+	Thread nit = new Thread(this);
+	private ArrayList<Figura> figure = new ArrayList<>();
+	static final Color[] boje = {Color.GREEN, Color.RED, Color.BLUE};
+	private int poeni = 0;
+	private Figura pokretna;
+	private Figura uPripravnosti;
+	private int nw, nh;
+
 	Tabla(int nw, int nh) {
 		this.nw = nw;
 		this.nh = nh;
+
+		uPripravnosti = nasumicnaFigura();
+		dodajFiguru(uPripravnosti);
+		postaviPokretnu();
+		nit.start();
 	}
 
-	private ArrayList<Figura> figure = new ArrayList<>();
-	void dodajFiguru(Figura f) {
-		figure.add(f);
-	}
+void ukloniFigure() {
+	figure.clear();
+	uPripravnosti = nasumicnaFigura();
+	dodajFiguru(uPripravnosti);
+	postaviPokretnu();
+}
 
-	{
-		dodajFiguru(new Jednodelni(new Pozicija(3,3), Color.RED));
-		dodajFiguru(new Dvodelni(new Pozicija(5, 5), Color.GREEN));
-	}
-
-	@Override 
-	public void paint(Graphics g) {
-		int W = this.getWidth();
-		int H = this.getHeight();
-		int w = W / nw;
-		int h = H / nh;
-
-		for (int i=0; i<=nw; ++i) {
-			//(i*w,0) -> (i*w,H);
-			g.drawLine(i*w, 0, i*w, H);
-		}
-
-		for (int i=0; i<=nh; ++i) {
-			//(0,i*h) -> (W,i*h)
-			g.drawLine(0, i*h, W, i*h);
-		}
-
-		for (Figura figura : figure) {
-			figura.naslikaj(g, this);
-		} 
-	}
-
-	boolean zauzeta(Pozicija p) throws Greska {
-		//(0,0) (1,0) ...(nw-1,0)
-		//(0,1) (1,1) ...
-		//(0,2) ...
-		//.
-		//.              (nw-1,nh-1);
-
-		if (p.x()<0 || p.x()>=nw || p.y()<0 || p.y()>=nh) {
-			throw new Greska("Pozicija van table!");
-		}
-
-		boolean z = false;
-		for (Figura f : figure) {
-			z |= f.zauzima(p);
-		}
-
-		return z;
-	}
-
-	private int nw, nh;
 	int nw() {
 		return nw;
 	}
@@ -73,5 +41,179 @@ class Tabla extends Canvas {
 
 	void postaviNh(int nh) {
 		this.nh = nh;
+	}
+
+void resetujPoene() {
+	poeni = 0;
+}
+
+	void pomeriPokretnu(Smer smer) {
+		try {
+			pokretna.pomeri(this, smer);
+		} catch (Greska g) {}
+	}
+
+	///////////////////////LABEL UPDATE////////////////
+
+	Label poeniLabel;
+	Label figureLabel;
+
+	void zabeleziLabele(Label x, Label y) {
+		this.poeniLabel = x;
+		this.figureLabel = y;
+	}
+
+	void azurirajLabele() {
+		poeniLabel.setText("Poeni: "+poeni);
+		figureLabel.setText("Figura: " + figure.size());
+	}
+
+	///////////////////////////////////////////////
+
+
+	Figura nasumicnaFigura() {
+		Pozicija p = new Pozicija(nw/2, 0);
+		Color boja = boje[(int)(Math.random()*boje.length)];
+		if (Math.random()<0.5) {
+			return new Jednodelni(p, boja);
+		} else {
+			return new Dvodelni(p, boja);
+		}
+	}
+
+	void postaviPokretnu() {
+		pokretna = uPripravnosti;
+		try {
+			pokretna.pomeri(this, Smer.Dole);
+		} catch (Greska g) {
+			//game over
+		}
+		uPripravnosti = nasumicnaFigura();
+		dodajFiguru(uPripravnosti);
+	}
+
+
+	void dodajFiguru(Figura f) {
+		figure.add(f);
+	}
+
+	void ukloniPopunjeneRedove() {
+		ArrayList<Integer> popunjeni = new ArrayList<>();
+		for (int i=0; i<nh; ++i) {
+			boolean popunjen = true;
+
+			for (int j=0; j<nw; ++j) {
+				Pozicija trenutna = new Pozicija(j, i);
+				try {
+					popunjen &= this.zauzeta(trenutna);
+				} catch (Greska g) {}
+			}
+
+			if (popunjen) {
+				popunjeni.add(i);
+			}
+		}
+
+		poeni += popunjeni.size() * 100;
+
+		ArrayList<Figura> zaBrisanje = new ArrayList<>();
+		for (Figura figura : figure) {
+			if (popunjeni.contains(figura.pozicija().y())) {
+				zaBrisanje.add(figura);
+			}
+		}
+
+		for (Figura figura : zaBrisanje) {
+			figure.remove(figura);
+		}
+
+		for (Figura figura : figure) {
+			if (figura!=pokretna) {
+				while (true) {
+					try {
+						figura.pomeri(this, Smer.Dole);
+					} catch (Greska g) {
+						break;
+					}
+				}
+			}
+		}
+
+		azurirajLabele();
+		if (popunjeni.size()!=0) {
+			ukloniPopunjeneRedove();
+		}
+	}
+
+	volatile boolean radi = true;
+
+	@Override
+	public void run() {
+		try {
+			while (!Thread.interrupted()) {
+				synchronized (this) {
+					if (!radi) {
+						wait();
+					}
+				}
+
+				try {
+					pokretna.pomeri(this, Smer.Dole);
+				} catch (Greska g) {
+					postaviPokretnu();
+					ukloniPopunjeneRedove();
+				}
+				repaint();
+
+				Thread.sleep(200);
+			}
+		} catch (InterruptedException e ){
+		}
+	}
+
+	synchronized void pokreni() {
+		radi = true;
+		notify();
+	}
+
+	void pauziraj() {
+		radi = false;
+	}
+
+	void prekini() {
+		nit.interrupt();
+	}
+
+	@Override 
+	public void paint(Graphics g) {
+		int W = this.getWidth();
+		int H = this.getHeight();
+		int w = W / nw;
+		int h = H / nh;
+
+		for (int i=0; i<=nw; ++i) {
+			g.drawLine(i*w, 0, i*w, H);
+		}
+
+		for (int i=0; i<=nh; ++i) {
+			g.drawLine(0, i*h, W, i*h);
+		}
+
+		for (Figura figura : figure) {
+			figura.naslikaj(g, this);
+		} 
+	}
+
+	boolean zauzeta(Pozicija p) throws Greska {
+		if (p.x()<0 || p.x()>=nw || p.y()<0 || p.y()>=nh) {
+			throw new Greska("Pozicija van table!");
+		}
+
+		boolean z = false;
+		for (Figura f : figure) {
+			z |= f.zauzima(p);
+		}
+
+		return z;
 	}
 }
--- zk2019/Tetris.java
+++ zk2019/Tetris.java
@@ -3,11 +3,57 @@ import java.awt.event.*;
 
 class Tetris extends Frame {
 	Tabla tabla = new Tabla(10, 30);
+
+	Label poeni = new Label("Poeni: 0", Label.CENTER);
+	Label figure = new Label("Figure: 0", Label.CENTER);
+
+	TextField x = new TextField();
+	TextField y = new TextField();
+	Button kreniStani = new Button("Stani|Kreni");
+
+	void novaIgra() {
+		try {
+			int nw = Integer.parseInt(x.getText());
+			int nh = Integer.parseInt(y.getText());
+			tabla.postaviNw(nw);
+			tabla.postaviNh(nh);
+			tabla.resetujPoene();
+			tabla.ukloniFigure();
+		} catch (NumberFormatException e) {
+		}
+	}
+
 	void dodajElemente() {
 		Panel panel = new Panel();
 		panel.setLayout(new GridLayout(1,1));
 		panel.add(tabla);
 		add(panel, "Center");
+
+		panel = new Panel();
+
+		panel.setLayout(new GridLayout(1, 2));
+
+		Panel panelL = new Panel();
+		panelL.setLayout(new GridLayout(2,1));
+		panelL.add(poeni);
+		panelL.add(figure);
+
+		panel.add(panelL);
+
+		Panel panelR = new Panel();
+		panelR.setLayout(new GridLayout(2, 1));
+		panelR.add(kreniStani);
+
+		Panel panelD = new Panel();
+		panelD.setLayout(new GridLayout(1,3));
+		panelD.add(new Label("x,y:"));
+		panelD.add(x);
+		panelD.add(y);
+		panelR.add(panelD);
+
+		panel.add(panelR);
+
+		add(panel, "South");
 	}
 
 	void dodajOsluskivace() {
@@ -17,14 +63,54 @@ class Tetris extends Frame {
 				dispose();
 			}
 		});
+
+		kreniStani.addActionListener((e)->{
+			if (tabla.radi) {
+				tabla.pauziraj();
+			} else {
+				tabla.pokreni();
+			}
+		});
+
+		tabla.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				switch (e.getKeyCode()) {
+					case KeyEvent.VK_LEFT:
+						tabla.pomeriPokretnu(Smer.Levo);
+						break;
+					case KeyEvent.VK_RIGHT:
+						tabla.pomeriPokretnu(Smer.Desno);
+						break;
+					case KeyEvent.VK_DOWN:
+						tabla.pomeriPokretnu(Smer.Dole);
+						break;
+				}
+			}
+		});
+	}
+
+	void dodajMeni() {
+		MenuBar bar = new MenuBar();
+		Menu menu = new Menu("Akacij");
+		setMenuBar(bar);
+		bar.add(menu);
+
+		MenuItem novaIgra = new MenuItem("Nova Igra", new MenuShortcut('N'));
+		menu.add(novaIgra);
+		novaIgra.addActionListener((e)->{
+			novaIgra();
+		});
 	}
 
 	Tetris() {
 		super("Tetris");
-		setSize(500, 500);
+		setSize(250, 600);
 		setVisible(true);
 		dodajElemente();
 		dodajOsluskivace();
+		dodajMeni();
+		tabla.zabeleziLabele(poeni, figure);
 	}
 
 	public static void main(String[] args) {
